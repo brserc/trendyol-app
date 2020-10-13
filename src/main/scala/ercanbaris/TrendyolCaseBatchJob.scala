0e239b84ed8b6361375ab5b0a8d3348ab0ba0717
barisercan <b.barisercan@gmail.com>
2020-10-13 23:30:52 +0300

    five operations working

--- src/main/scala/ercanbaris/TrendyolCaseBatchJob.scala
+++ src/main/scala/ercanbaris/TrendyolCaseBatchJob.scala
@@ -1,6 +1,7 @@
 package ercanbaris
 
 import org.apache.flink.api.scala._
+import org.apache.flink.util.Collector
 import java.nio.file.Paths
 import ercanbaris.UserAction
 
@@ -15,5 +16,18 @@ object TrendyolCaseBatchJob {
     val uniqueevents = data.map{ua => (ua.eventName,1)}.groupBy(0).sum(1)
     uniqueevents.print
 
+    val userWithAllEvents = data.map{ua =>(ua.userId,ua.eventName)}.groupBy(0).reduceGroup {
+      (in, out: Collector[(Int, String)]) =>
+        in.toSet foreach (out.collect)
+    }.map{ua => (ua._1,ua._2,1)}.groupBy(0).sum(2).filter{_._3 == 4}
+    userWithAllEvents.print
+
+    val allEventsUserId = data.filter{_.userId == 47}.map{ua => (ua.eventName,1)}.groupBy(0).sum(1)
+    allEventsUserId.print
+
+    val productViewsUserId = data.filter{_.userId == 47}.map{ua => (ua.productId)}
+    productViewsUserId.print
   }
 }
+
+//.filter{_._1 != 4}
