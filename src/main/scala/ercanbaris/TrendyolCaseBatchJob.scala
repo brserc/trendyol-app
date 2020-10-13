f1cda280c10be2c5546439660534eb839553a526
barisercan <b.barisercan@gmail.com>
2020-10-13 22:35:10 +0300

    distinct product and unique event computation

--- src/main/scala/ercanbaris/TrendyolCaseBatchJob.scala
+++ src/main/scala/ercanbaris/TrendyolCaseBatchJob.scala
@@ -9,7 +9,11 @@ object TrendyolCaseBatchJob {
 
     val env = ExecutionEnvironment.getExecutionEnvironment
     val data: DataSet[UserAction] = env.readCsvFile[UserAction](filePath = "./src/main/resources/case.csv",fieldDelimiter = "|",ignoreFirstLine = true)
-    data.print
+    val distinctproduct = data.filter{_.eventName == "view"}.map{ua => (ua.productId,1)}.groupBy(0).sum(1)
+    distinctproduct.print
+
+    val uniqueevents = data.map{ua => (ua.eventName,1)}.groupBy(0).sum(1)
+    uniqueevents.print
 
   }
 }
