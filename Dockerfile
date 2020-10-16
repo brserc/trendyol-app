007967986373ed73d53af4b82e69fce289cdc36f
Baris Ercan <b.barisercan@gmail.com>
2020-10-16 22:30:30 +0300

    save all outputs to csv, fix one element tuple error

--- Dockerfile
+++ Dockerfile
@@ -3,4 +3,5 @@ FROM flink:1.11.2-scala_2.12-java11
 COPY ./src/main/resources/case.csv /trendyol_docker/
 RUN chown -R flink:flink /trendyol_docker/
 COPY ./target/scala-2.12/trendyol-app-assembly-0.1-SNAPSHOT.jar /trendyol_docker/
+RUN mkdir -p /trendyol_docker/src/main/resources/results/
 RUN ls -la /trendyol_docker
--- src/main/scala/ercanbaris/TrendyolCaseBatchJob.scala
+++ src/main/scala/ercanbaris/TrendyolCaseBatchJob.scala
@@ -6,13 +6,14 @@ import org.apache.flink.util.Collector
 object TrendyolCaseBatchJob {
   def main(args: Array[String]): Unit = {
     val env = ExecutionEnvironment.getExecutionEnvironment
-    //val data: DataSet[UserAction] = env.readCsvFile[UserAction](filePath = "src/main/resources/case.csv",fieldDelimiter = "|",ignoreFirstLine = true)
     val data: DataSet[UserAction] = env.readCsvFile[UserAction](filePath = "/trendyol_docker/case.csv",fieldDelimiter = "|",ignoreFirstLine = true)
 
-
-
+    DataProcessor.uniqueProductViewByProductId(data).writeAsCsv("./main/resources/results/uniqueProductViewByProductId.csv", "\n", "|");
+    DataProcessor.uniqueEventCounts(data).writeAsCsv("./main/resources/results/uniqueEventCounts.csv", "\n", "|");
+    DataProcessor.topFiveFulfilledAllEvents(data).writeAsCsv("./main/resources/results/topFiveFulfilledAllEvents.csv", "\n", "|");
+    DataProcessor.allEventsOfUser(data,47).writeAsCsv("./main/resources/results/allEventsOfUser.csv", "\n", "|");
+    DataProcessor.productViewsOfUser(data,47).writeAsCsv("./main/resources/results/productViewsOfUser.csv", "\n", "|");
 
   }
 }
 
-//.filter{_._1 != 4}
